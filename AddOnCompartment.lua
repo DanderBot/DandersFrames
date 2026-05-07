c4e53ac619d1a76290efb15b1112c0bd5efd40a9
JPEscher <JPEscher@gmail.com>
2026-05-07 05:34:46 -0400

    got solo mode toggle working

--- AddOnCompartment.lua
+++ AddOnCompartment.lua
@@ -4,6 +4,11 @@ local db = DF:GetDB()                                                   -- Get t
 DF.L = LibStub("AceLocale-3.0"):GetLocale("DandersFrames")              -- Get the localization
 local L = DF.L                                                          -- Set the localization
 
+--[[
+	NOTE :: The addon compartment is registered on addon_loaded. The functoin for right click needs the profile for the character.
+	This is why we regrab the profile. Anything needing a specific profile needs the profile grabbed on execution!
+--]]
+
 if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then                        -- If on Retail (this is only supported on 10.x and up
     -- if [settings check here] then                                    -- Optional check against a setting to enable this
         AddonCompartmentFrame:RegisterAddon({                           -- start adding to the addon compartment
@@ -14,10 +19,9 @@ if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then                        -- If on
                 if menuInputData.buttonName == "LeftButton" then        -- if a left click
                     DF:ToggleGUI()                                      -- toggle GUI vibility
                 elseif menuInputData.buttonName == "RightButton" then   -- if a right click
+					local db = DF:GetDB()                               -- Get the DB Settings for loaded player
 					if db.soloMode ~= nil then                          -- if solo mode is set
-						if db.soloMode then print("solomode is set to true") else print("solomode is set to false") end -- debug line; remove post debug
 						db.soloMode = not db.soloMode                   -- switch the setting
-						if db.soloMode then print("solomode is set to true") else print("solomode is set to false") end -- debug line; remove post debug
 						DF:UpdateAllFrames()                            -- update frames || this is not honored for some reason || needs incestigation
 						print("|cff00ff00DandersFrames:|r " .. format(L["Solo mode %s"], db.soloMode and L["enabled"] or L["disabled"]))
 					end
